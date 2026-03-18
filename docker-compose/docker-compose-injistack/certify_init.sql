9b31a5ce74cda4c336b4b837aaec4baaaca75003
kysztech <yogesh@kysz.tech>
2026-03-18 18:54:26 +0530

    Update DID URLs in SQL and properties files, modify token endpoints in Mimoto issuers configuration to use service URIs

--- docker-compose/docker-compose-injistack/certify_init.sql
+++ docker-compose/docker-compose-injistack/certify_init.sql
@@ -181,7 +181,7 @@ VALUES (
     'https://www.w3.org/2018/credentials/v1',  -- context as comma-separated string
     'FarmerCredential,VerifiableCredential',  -- credential_type as comma-separated string
     'ldp_vc',  -- credential_format
-    'did:web:mosip.github.io:inji-config:vc-local-ed25519',  -- did_url
+    'did:web:kysz-parth.github.io:inji-certify:vc-local-ed25519',  -- did_url
     'CERTIFY_VC_SIGN_ED25519',  -- key_manager_app_id
     'ED25519_SIGN',  -- key_manager_ref_id (optional)
     'EdDSA',  -- signature_algo (optional)
